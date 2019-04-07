8f34c902f231d87353a90af7b69811c441b54778
Łukasz Wójcik <lwojcik@lukem.net>
2019-04-07 23:42:18 +0200

    coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -17,10 +17,10 @@ module.exports = {
   "collectCoverage": true,
   "coverageThreshold": {
     "global": {
-      "branches": 0,
-      "functions": 0,
-      "lines": 0,
-      "statements": 0
+      "branches": 100,
+      "functions": 100,
+      "lines": 100,
+      "statements": 100
     }
   }
 }
