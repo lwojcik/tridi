fb0030d926bf952ab19175daf6edf3706ea9eb83
Łukasz Wójcik <lwojcik@lukem.net>
2019-04-07 19:20:43 +0200

    more tests

--- tests/tridi.test.ts
+++ tests/tridi.test.ts
@@ -5,6 +5,10 @@ console = <any>{
   error: jest.fn()
 }
 
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
 const setupTridi = (containerId: string, options: object) => {
   const tridiContainer = document.createElement('div');
   tridiContainer.id = containerId;
@@ -106,6 +110,13 @@ describe('Tridi.load()', () => {
 describe('Init options validation', () => {
   const containerId = 'tridi-test-container-3';
 
+  const commonValidOptions = {
+    element: `#${containerId}`,
+    location: './images/1',
+    format: 'jpg',
+    count: 5,
+  }
+
   test(`should throw for missing 'element' property`, () => {
     const options = {
       location: './images/1',
@@ -118,17 +129,155 @@ describe('Init options validation', () => {
 
   test(`should throw when 'images' and 'format' properties are missing`, () => {
     const options = {
+      element: `#${containerId}`,
       count: 5
     };
 
     expect(() => setupTridi(containerId, options).load()).toThrow();
   });
 
-  test(`should throw when setting images as 'numbered' and 'location' is missing`, () => {
+  test(`should call console.error when 'numbered' and 'location' is missing`, () => {
     const options = {
+      element: `#${containerId}`,
       images: 'numbered'
     };
 
-    expect(() => setupTridi(containerId, options).load()).toThrow();
+    setupTridi(containerId, options).load();
+
+    expect(console.error).toHaveBeenCalledWith(`'location' property is missing or invalid. Image location must be provided for 'numbered' property.`);
+  });
+
+  test(`should accept array of strings as image source`, () => {
+    const imgArray = [
+      './images/1/1.jpg',
+      './images/1/2.jpg',
+      './images/1/3.jpg'
+    ];
+
+    const options = {
+      element: `#${containerId}`,
+      images: imgArray
+    };
+
+    expect(() => setupTridi(containerId, options).load()).not.toThrow();
+  });
+
+  test(`should accept 'draggable' option`, () => {
+    const options = {
+      ...commonValidOptions,
+      draggable: true
+    };
+
+    expect(() => setupTridi(containerId, options).load()).not.toThrow();
+  });
+
+  test(`should accept 'autoplaySpeed' option`, () => {
+    const options = {
+      ...commonValidOptions,
+      autoplaySpeed: 2,
+    };
+
+    expect(() => setupTridi(containerId, options).load()).not.toThrow();
+  });
+
+  test(`should accept 'passive' option`, () => {
+    const options = {
+      ...commonValidOptions,
+      passive: true,
+    };
+
+    expect(() => setupTridi(containerId, options).load()).not.toThrow();
+  });
+
+  test(`should accept 'touch' option`, () => {
+    const options = {
+      ...commonValidOptions,
+      touch: true,
+    };
+
+    expect(() => setupTridi(containerId, options).load()).not.toThrow();
+  });
+
+  test(`should accept 'mouseleaveDetect' option`, () => {
+    const options = {
+      ...commonValidOptions,
+      mouseleaveDetect: true,
+    };
+
+    expect(() => setupTridi(containerId, options).load()).not.toThrow();
+  });
+
+  test(`should accept 'autoplay' option`, () => {
+    const options = {
+      ...commonValidOptions,
+      autoplay: true,
+      stopAutoplayOnClick: true,
+      stopAutoplayOnMouseenter: true,
+      resumeAutoPlayOnMouseleave: true,
+    };
+
+    expect(() => setupTridi(containerId, options).load()).not.toThrow();
+  });
+});
+
+describe('Behavior', () => {
+  const containerId = 'tridi-test-container-4';
+
+  const options = {
+    element: `#${containerId}`,
+    location: './images/1',
+    format: 'jpg',
+    count: 5,
+  }
+  
+  const tridiInstance = setupTridi(containerId, options);
+  tridiInstance.load();
+
+  test('prev() should not throw', () => {
+    expect(() => tridiInstance.prev()).not.toThrow();
+  });
+
+  test('next() should not throw', () => {
+    expect(() => tridiInstance.next()).not.toThrow();
+  });
+
+  test('autoplayStart() should not throw', () => {
+    expect(() => tridiInstance.autoplayStart()).not.toThrow();
+  });
+
+  test('autoplayStop() should not throw', () => {
+    expect(() => tridiInstance.autoplayStop()).not.toThrow();
+  });
+});
+
+describe('Update', () => {
+  const containerId = 'tridi-test-container-5';
+
+  const options = {
+    element: `#${containerId}`,
+    format: 'jpg',
+    count: 5,
+  }
+
+  const oldOptions = {
+    ...options,
+    location: './images/1',
+  }
+
+  const newOptions = {
+    ...options,
+    location: './images/2',
+  }
+
+  
+  const tridiInstance = setupTridi(containerId, oldOptions);
+  tridiInstance.load();
+
+  test('should not throw', () => {
+    expect(() => tridiInstance.update(newOptions)).not.toThrow();
+  });
+
+  test(`should accept 'syncFrame' parameter`, () => {
+    expect(() => tridiInstance.update(newOptions, true)).not.toThrow();
   });
 });
