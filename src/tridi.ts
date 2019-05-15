f7a6f8254a24e12c34d27573ea18e16f67b8eac1
Łukasz Wójcik <lwojcik@lukem.net>
2019-05-15 19:18:41 +0200

    v1.1.1

--- src/tridi.ts
+++ src/tridi.ts
@@ -1,5 +1,5 @@
 /*
-  Tridi v1.1.0 - JavaScript 360 3D Product Viewer
+  Tridi v1.1.1 - JavaScript 360 3D Product Viewer
   Author: Lukasz Wojcik
   License: MIT
   Homepage: https://tridi.lukem.net
